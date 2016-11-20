10125568762e4d2a68c9eb55fa716827d0652f53
Victor Klimov <4klimov@gmail.com>
2016-11-20 21:20:13 +0300

    Edit 2 for init.sh and nginx.conf

--- web_server/init.sh
+++ web_server/init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo apt-get update
 sudo apt-get install nginx
 cd ~
 mkdir web
@@ -6,8 +7,10 @@ cd web
 mkdir public uploads etc
 cd public
 mkdir img css js
-
+cd ~
+sudo rm -rf /etc/nginx/sites-enabled/default /etc/nginx/sites-available/default
 cp ~/web_server/nginx.conf /home/box/web/etc/nginx.conf
 
-sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo /etc/init.d/nginx start﻿
+sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+# sudo /etc/init.d/nginx start﻿
+sudo nginx
